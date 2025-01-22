16a223aeda27127f6824899e62c9b3d7aedda22f
hiteshsharma91 <hiteshsit11@gmail.com>
2025-01-22 10:13:58 +0530

    add 02 and 03 file

---
+++ 02_add_twoNumbers.py
@@ -0,0 +1,11 @@
+#method 1--->
+num1 = 30
+num2 = 20
+
+print("sum of these no. is : ", num1+num2)
+
+#METHOD 2 --->>
+num1 = int(input("enter the 1st no.: "))
+num2 = int(input("enter the 1st no.: "))
+
+print("sum of entered no.: ",num1+num2)
---
+++ 03_find_squreRoot.py
@@ -0,0 +1,4 @@
+num1 = float(input("enter the no. : "))
+sq = num1**(0.5) #we can take power of num (1/2)
+
+print("squreRoot of entered no.: ",sq)
