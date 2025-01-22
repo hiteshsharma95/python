8e1d29fd66670f203ca63e6c24e21e6a1dd09b0e
hiteshsharma91 <hiteshsit11@gmail.com>
2025-01-22 10:37:50 +0530

    add 2nd method

--- 03_find_squreRoot.py
+++ 03_find_squreRoot.py
@@ -2,3 +2,11 @@ num1 = float(input("enter the no. : "))
 sq = num1**(0.5) #we can take power of num (1/2)
 
 print("squreRoot of entered no.: ",sq)
+
+#METHOD 2 (USING MATH MODULE)--->>
+
+import math
+num1 = float(input("enter the no.:" ))
+sq = math.sqrt(num1)
+
+print("squareRoot of this no. is : ",sq)
